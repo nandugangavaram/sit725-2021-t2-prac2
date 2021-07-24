acef532d73928a184a702882f6c22c6df0b55e22
nandugangavaram <nandugangavaram@gmail.com>
2021-07-24 22:49:09 +0530

    Added PUT Method with empty inputs validation, Updated README.MD and Updated idex.html with PUT Method

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # sit725-2021-t2-prac2
+
+This is a project to demostrate use of express to create end-points for adding and retrieving contacts from the server. 
+
+Use of Methods:
+1.  **GET** - `/getCustomer`
+
+    *Inputs: - **Mandatory***
+
+        1. name 
+---        
+2.  **POST** - `/addContact`
+
+    *Inputs: - **Mandatory***
+
+        1. name 
+        2. phone 
+---
+3.  **PUT** - `/updateContact`
+
+    *Inputs: - **Mandatory*** 
+
+        1. name 
+        2. phone 
+---
+
+# Running the application
+
+1.  You can manually download the project from Github **(OR)** If you have git bash installed use command **`git clone https://github.com/nandugangavaram/sit725-2021-t2-prac2`** in terminal to replicate the repository to your local system.
+
+2. Open the folder where the repository is cloned in visual studio code and open terminal and type **`npm install`** to install all dependencies.
+
+3. After completion of previous step, in terminal type **`node server.js`** to run the server on predefined-env port or 3000 (Default).
+
+4. Navigate to the hoster server in your preferred browser, eg. **`http:localhost:3000`**. List of available end-points are displayed. 
+
+5. Now, end-points defined in initial landing page **(OR)** one's mentioned above can be used to trigger end-points.
+
+6. `Riskman` or any other similar software applications can be used to trigger end-points directly once the server has started successfully.
+
+# Copyrights
+
+This is a public project that means anyone can use this project for their own testing and work.
+
+Copyright (c) 2021 Gangavaram Gouri Nandan Reddy
--- server.js
+++ server.js
@@ -11,25 +11,46 @@ var contacts = {}
 
 app.get('/getContact', (req, res) => {
     let name = req.query.name;
-    if(name in contacts) {
-        res.status(200);
-        res.send("Contact number for " + name + " person is " + contacts[name]);
+    if(name) {
+        if(name in contacts) {
+            res.status(200);
+            res.send("Contact number for " + name + " person is " + contacts[name]);
+        } else {
+            res.status(404);
+            res.send("Contact not found, call /addContact - POST Method to create contact.");
+        }
     } else {
-        res.status(400);
-        res.send("Contact not found!");
+        res.json({statusCode: 404, message: "Input Missing, please check input and try again!"});
     }
 });
 
 app.post('/addContact', (req, res) => {
     let name = req.body.name;
     let phone = req.body.phone;
-    
-    if(!(name in contacts)) {
-        contacts[name] = phone;
-        res.json({statusCode: 200, data: {name, phone}, message: "Added Successfully"});
-    } else {        
-        contacts[name] = phone;
-        res.json({statusCode: 200, data: {name, phone}, message: "Updated Successfully"});
+    if(name && phone) {
+        if(!(name in contacts)) {
+            contacts[name] = phone;
+            res.json({statusCode: 201, data: {name, phone}, message: "Added Successfully."});
+        } else {
+            res.json({statusCode: 409, data: {name, phone}, message: "Contact Already Exists, call /updateContact - PUT Method to update contact."});
+        }
+    } else {
+        res.json({statusCode: 404, message: "Inputs Missing, please check inputs and try again!"});
+    }
+});
+
+app.put('/updateContact', (req, res) => {
+    let name = req.body.name;
+    let phone = req.body.phone;
+    if(name && phone) {
+        if(name in contacts) {
+            contacts[name] = phone;
+            res.json({statusCode: 200, data: {name, phone}, message: "Updated Successfully."});
+        } else {        
+            res.json({statusCode: 404, data: {name, phone}, message: "Contact not found, call /addContact - POST Method to create contact."});
+        }
+    } else {
+        res.json({statusCode: 404, message: "Inputs Missing, please check inputs and try again!"});
     }
 });
 
