f77e26eaf2752b0d1d8e29992b1e0a93b86c3e26
nandugangavaram <nandugangavaram@gmail.com>
2021-07-24 19:41:18 +0530

    Added Post method for adding contacts (/addContact)

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 var port = process.env.PORT || 3000;
 
+var contacts = {}
+
+app.post('/addContact', (req, res) => {
+    let name = req.body.name;
+    let phone = req.body.phone;
+    
+    if(!(name in contacts)) {
+        contacts[name] = phone;
+        res.json({statusCode: 200, data: {name, phone}, message: "Added Successfully"});
+    } else {        
+        contacts[name] = phone;
+        res.json({statusCode: 200, data: {name, phone}, message: "Updated Successfully"});
+    }
+});
 
 app.listen(port, () => {
     console.log("Application started & listening on port : " + port);
