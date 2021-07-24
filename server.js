0f2464ffac73e374b09420ccbbee3cdd59afa31a
nandugangavaram <nandugangavaram@gmail.com>
2021-07-24 19:41:56 +0530

    Added Get method for getting contacts (/getContact)

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ var port = process.env.PORT || 3000;
 
 var contacts = {}
 
+app.get('/getContact', (req, res) => {
+    let name = req.query.name;
+    if(name in contacts) {
+        res.status(200);
+        res.send("Contact number for " + name + " person is " + contacts[name]);
+    } else {
+        res.status(400);
+        res.send("Contact not found!");
+    }
+});
+
 app.post('/addContact', (req, res) => {
     let name = req.body.name;
     let phone = req.body.phone;
